d35b1c12005b1303bd62ea9ef66575475669d4a0
unknown <aminsalkovic2019@gmail.com>
2022-02-07 14:00:45 +0100

    //

---
+++ vezba25.js
@@ -0,0 +1,22 @@
+
+// function vezba(ime){
+//  return ime.split('').reverse().join('')
+
+// }
+
+// console.log(vezba("")) 
+
+// function vezba(ime){
+//     const arr=ime.split('')
+//     const f=arr[0][0].toUpperCase()
+//     const p=arr[1][0].toUpperCase()
+//     const newString=f+"."+p
+//     return newString
+//  }
+// console.log(vezba("amin")) 
+
+names="amin"
+for(i=0;i<names.length;i++){
+    
+    console.log(names[i].toUpperCase([0][0]))
+}
