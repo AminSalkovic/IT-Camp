81443c989bd91403d8a573b8af85275fc051eb6b
unknown <aminsalkovic2019@gmail.com>
2022-03-09 19:02:21 +0100

    nesto

--- vezba26.js
+++ vezba26.js
@@ -586,4 +586,11 @@
 // }
 // getData();
 
-
+// function getAverage(marks){
+//   let sum=0
+//     for(i=0;i<marks.length;i++){
+//          sum+=marks[i]
+//     }
+//     return Math.floor(sum/marks.length)
+// }
+// console.log(getAverage([1,2,3,4,5,]));
