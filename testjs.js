6e5c55c78eb9d6d7c2dbf190221b4171d0f09481
unknown <aminsalkovic2019@gmail.com>
2022-04-05 18:08:22 +0200

    testjs

---
+++ testjs.js
@@ -0,0 +1,24 @@
+async function facts(){
+    const dugme=document.getElementById("dugme")
+    const response = await fetch("https://catfact.ninja/facts")
+    const response2 = await response.json()
+      const  response3=response2.data
+         response3.forEach((el) => {
+        element=document.createElement("p")
+         element.innerText=el.fact
+         dugme.appendChild(element)
+        });
+}
+
+console.log(facts());
+
+
+// async function some(){
+//     const dugme=document.getElementById("dugme")
+//     const response = await fetch("https://catfact.ninja/facts")
+//     const response2 = await response.json()
+//     console.log(response2);
+    
+// }
+
+// console.log(some());
